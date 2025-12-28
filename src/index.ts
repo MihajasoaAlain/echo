4be8fa0b96e193b869ef8b80889de57edfb5b145
Mihajasoa <mihajasoaalain85@gmail.com>
2025-12-28 16:52:05 +0300

    add read system

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,15 @@ import { createAdapter } from '@socket.io/redis-adapter';
 import { pushNotificationSchema } from './schemas/notification.schema';
 import { prisma } from './lib/prisma';
 import { authenticateApiKey } from './middleware/auth.middleware';
-import { success } from 'zod';
-import { id } from 'zod/locales';
 import { rateLimiter } from './middleware/rate-limiter.middleware';
+import cors from '@fastify/cors';
 
 const server = Fastify({ logger: true });
+server.register(cors, {
+  origin: true, 
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'x-api-key'],
+});
 
 const pubClient = new Redis({ host: 'localhost', port: 6379 });
 const subClient = pubClient.duplicate();
@@ -41,6 +45,60 @@ server.ready((err) => {
 
 server.get('/health', async () => {
   return { status: 'OK' };
+});
+
+server.get('/api/v1/notifications/:userId',{
+  preHandler:[authenticateApiKey]
+},async (request, reply) => {
+  const { userId } = request.params as { userId: string };
+
+  const notifications = await prisma.notification.findMany({
+    where: { userId:userId,
+      isRead: false,
+     },
+    orderBy: { createdAt: 'desc' },
+    take: 50,
+  });
+
+  return { notifications };
+});
+
+// 1. Marquer TOUT comme lu pour un utilisateur
+server.patch('/api/v1/notifications/read-all/:userId', {
+  preHandler: [authenticateApiKey]
+}, async (request, reply) => {
+  const { userId } = request.params as { userId: string };
+
+  const result = await prisma.notification.updateMany({
+    where: {
+      userId: userId,
+      isRead: false
+    },
+    data: {
+      isRead: true
+    }
+  });
+
+  return { 
+    message: `${result.count} notifications marquées comme lues.`,
+    count: result.count 
+  };
+});
+
+server.patch('/api/v1/notifications/:id/read', {
+  preHandler: [authenticateApiKey]
+}, async (request, reply) => {
+  const { id } = request.params as { id: string };
+
+  try {
+    await prisma.notification.update({
+      where: { id: id },
+      data: { isRead: true }
+    });
+    return { success: true, message: "Notification mise à jour." };
+  } catch (error) {
+    reply.status(404).send({ error: "Notification non trouvée." });
+  }
 });
 
 server.post('/api/v1/push',{
